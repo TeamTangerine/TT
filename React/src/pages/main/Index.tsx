fe142a53053fe308cd1b51bd960844fed6870e18
MinKyeonghyeon <28599@naver.com>
2025-09-01 23:47:03 +0900

    chore/컴퐅넌트 작성 예시

---
+++ React/src/pages/main/Index.tsx
@@ -0,0 +1,15 @@
+import HowTo from "./components/HowTo"
+
+
+function Main (){
+ return( 
+ <>
+  <h1>
+      이 페이지는 이해를 돕기위해 작성된 예시 페이지 입니다.  </h1>
+      {/* 컴포넌트 안에 컴포넌트를 불러오는 예시입니다. */}
+      <HowTo title="사용방법" age={22}/>
+ </>
+    
+)
+}
+export default Main
