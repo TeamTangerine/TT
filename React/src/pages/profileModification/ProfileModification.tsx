57c01dd6f83cf0a1f43ce3faef8ae9603a42ba9b
gyeone <voov000@naver.com>
2025-09-04 13:14:57 +0900

    refactor/ 중복되는 태그들 컴포넌트로 변경

--- React/src/pages/profileModification/ProfileModification.tsx
+++ React/src/pages/profileModification/ProfileModification.tsx
@@ -1,5 +1,7 @@
 import Header from '../../components/Header';
 import basicProfileImg from '../../assets/basic-profile-img.png';
+import ImgBtn from '../../assets/img-button.png';
+import TextInput from './components/TextInput';
 
 function ProfileModification() {
   return (
@@ -7,16 +9,14 @@ function ProfileModification() {
       <Header />
       <div>
         <img src={basicProfileImg} alt="사용자 프로필 이미지" />
-        {/* 버튼 이미지는 배경 이미지로 넣기 */}
-        <button></button>
+        <button>
+          <img src={ImgBtn} alt="프로필 이미지 변경 버튼" />
+        </button>
       </div>
       <form>
-        <label htmlFor="userName">사용자 이름</label>
-        <input id="userName" type="text" placeholder="2~10자 이내여야 합니다." />
-        <label htmlFor="accountId">계정 ID</label>
-        <input id="accountId" type="text" placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다." />
-        <label htmlFor="introduce">소개</label>
-        <input id="introduce" type="text" placeholder="자신과 판매할 상품에 대해 소개해 주세요!" />
+        <TextInput id="userName" content="사용자 이름" placeholder="2~10자 이내여야 합니다." />
+        <TextInput id="accountId" content="계정 ID" placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다." />
+        <TextInput id="userName" content="소개" placeholder="자신과 판매할 상품에 대해 소개해 주세요!" />
       </form>
     </>
   );
