7fdecc3f9db420661b5c305db9adce62bec646d9
Liebe_Chaeso <d131215@naver.com>
2025-09-04 10:43:01 +0900

    feat/login 해당 작업을 할 branch 생성

---
+++ React/src/Login/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SymbolLogoGray from '../assets/symbol-logo-gray.png';
+
+export default function Login() {
+  return (
+    <div>
+      <div>
+        <img src={SymbolLogoGray} className="width" alt="감귤마켓로고" />
+      </div>
+    </div>
+  );
+}
