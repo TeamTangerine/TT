e48a0d5d9b78d22442ca72d8b89b936a240e9e1a
Liebe_Chaeso <d131215@naver.com>
2025-09-04 15:25:13 +0900

    feat/login: 로그인창 마크업 및 디자인 작업완료
    로그인창 마크업 및 디자인 작업 완료 / 카카오톡 로고 이미지파일 누락->추가

---
+++ React/src/Login/components/EmailLogin.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+type EmailLoginProps = {
+  name: string;
+  logoImg: string;
+  borderColor: string;
+  altText: string;
+};
+
+export default function EmailLogin({ name, logoImg, borderColor, altText }: EmailLoginProps) {
+  return (
+    <>
+      <button
+        className={`relative w-[322px] h-[44px] rounded-[44px] border-[1px] ${borderColor} text-[14px] text-[#767676] text-center flex items-center justify-center cursor-pointer`}
+      >
+        <img src={logoImg} alt={altText} className="absolute left-[14px] w-[24px] h-[24px]" />
+        <span>{name} 계정으로 로그인</span>
+      </button>
+    </>
+  );
+}
