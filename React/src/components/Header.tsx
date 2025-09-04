e4609f83ea41de608b1e733f0451c1d93b3128fc
MinKyeonghyeon <28599@naver.com>
2025-09-04 12:08:15 +0900

    feat/ Header 컴포넌트 마크업 및 스타일링 작성

--- React/src/components/Header.tsx
+++ React/src/components/Header.tsx
@@ -1,4 +1,15 @@
+import iconSearch from '../assets/icon/icon-search.png';
+
 function Header() {
-  return <h1>여기는 헤더입니다.</h1>;
+  return (
+    <nav className="flex justify-center w-full h-[48px] border-b border-b-[#DBDBDB] px-[16px] py-[12px]">
+      <div className="flex justify-between items-center w-full max-w-[384px]">
+        <h1 className="text-lg">감귤마켓 피드</h1>
+        <button>
+          <img src={iconSearch} alt="검색" />
+        </button>
+      </div>
+    </nav>
+  );
 }
 export default Header;
