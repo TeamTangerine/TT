b309a350aa9547bf4fb497337899ba10e067161a
MinKyeonghyeon <28599@naver.com>
2025-09-05 16:02:35 +0900

    fix/넘치는 부분 잘라내고 너비를 고정

--- React/src/pages/yourprofile/component/ProductList.tsx
+++ React/src/pages/yourprofile/component/ProductList.tsx
@@ -2,16 +2,18 @@ import Product from './Product';
 
 function ProductList() {
   return (
-    <section className="w-full pl-5 py-4 flex flex-col gap-4 bg-white">
-      <h2>판매 중인 상품</h2>
-      <ul className="flex gap-[10px] overflow-hidden">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-      </ul>
+    <section className="w-full flex justify-center pl-5 py-4  bg-white">
+      <div className="flex flex-col gap-4">
+        <h2 className="font-bold">판매 중인 상품</h2>
+        <ul className="flex gap-[10px] max-w-[384px] overflow-hidden">
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </ul>
+      </div>
     </section>
   );
 }
