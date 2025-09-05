9ca2358d780576f45d75a448226df7240d9bee34
Liebe_Chaeso <d131215@naver.com>
2025-09-05 13:32:26 +0900

    feat/membership: 로그인, 회원가입 관리 폴더이름 수정 및 JoinMembership.tsx 완성
    1. 로그인 및 회원가입 관리 폴더이름 수정(Login -> Auth)
    
    2. JoinMembership.tsx 파일 작성완료 및 회원가입 폼에 맞는 태그 추가
    추가내용)
    <span className="text-[12px] text-[#EB5757] mt-[6px]">*이미 가입된 이메일 주소입니다.</span>
    <span className="text-[12px] text-[#EB5757] mt-[6px]">*이메일 또는 비밀번호가 일치하지 않습니다.</span>
    
    해당 내용들 작성후 주석처리

---
+++ React/src/pages/Auth/JoinMembership.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoginSignUpForm from './components/LoginSignUpForm';
+
+export default function JoinMembership() {
+  return (
+    <div>
+      <LoginSignUpForm formName="이메일로 회원가입" btnText="다음" />
+    </div>
+  );
+}
