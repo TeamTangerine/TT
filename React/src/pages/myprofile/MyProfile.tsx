bcb520acb2407703f6c9a6c8179b1ed93a5afff7
MinKyeonghyeon <28599@naver.com>
2025-09-05 16:02:57 +0900

    fix/컴포넌트 사이에 간격을 넣어 피그마와 디자인 통일

--- React/src/pages/myprofile/MyProfile.tsx
+++ React/src/pages/myprofile/MyProfile.tsx
@@ -1,5 +1,5 @@
 import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import Footer from '../../components/Footer/Footer';
 import UserInfo from '../yourprofile/component/UserInfo';
 import ProductList from '../yourprofile/component/ProductList';
 import PostingGrid from '../../components/PostingGrid';
@@ -8,9 +8,12 @@ function MyProfile() {
   return (
     <>
       <Header />
-      <UserInfo />
-      <ProductList />
-      <PostingGrid />
+      <main className="pb-[60px] flex flex-col gap-[6px] bg-[#f2f2f2]">
+        <UserInfo />
+        {/* 버튼을 피그마 디자인에 맞게 동적 할당하는 로직 추가 필요합니다! */}
+        <ProductList />
+        <PostingGrid />
+      </main>
       <Footer />
     </>
   );
