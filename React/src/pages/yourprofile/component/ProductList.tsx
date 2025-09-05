7a2670baddb3f7b78d8541b6b77444698499d2bd
MinKyeonghyeon <28599@naver.com>
2025-09-05 16:29:39 +0900

    fix/간격 뒤바뀐거 수정

--- React/src/pages/yourprofile/component/ProductList.tsx
+++ React/src/pages/yourprofile/component/ProductList.tsx
@@ -2,7 +2,7 @@ import Product from './Product';
 
 function ProductList() {
   return (
-    <section className="w-full flex justify-center pl-5 py-4  bg-white">
+    <section className="w-full flex justify-center pl-4 py-5  bg-white">
       <div className="flex flex-col gap-4">
         <h2 className="font-bold">판매 중인 상품</h2>
         <ul className="flex gap-[10px] max-w-[384px] overflow-hidden">
