93d49b2d053683f2d40f915a45f5b653aec2496a
gyeone <voov000@naver.com>
2025-09-05 13:19:28 +0900

    comment/모달 기능 설명 주석 추가

--- React/src/components/modal/Modal.tsx
+++ React/src/components/modal/Modal.tsx
@@ -5,7 +5,8 @@ function Modal() {
     <>
       <img src={modalBarImg} alt="모달 바" />
       <ul>
-        <li>삭제하기</li>
+        {/* li는 기능 작업 시 props를 받아와서 조건부 렌더링으로 switch문으로 처리하기 (switch문 => li를 컴포넌트로 안 만들어도 됨) */}
+        <li></li>
       </ul>
     </>
   );
