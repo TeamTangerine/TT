e1059f4d07f4440670caeb5d153394a2424ca5ba
Liebe_Chaeso <d131215@naver.com>
2025-09-06 10:30:30 +0900

    feat/button: button interface 정의, 각 속성들을 enum으로 정의하는 작업
    1. button의 interface를 분류했습니다.
    
    - 버튼 텍스트 내용, 버튼 텍스트 크기, 버튼의 width, 버튼 height, 버튼 radius, 버튼의 배경색
    - 총 6개의 타입을 정의했습니다.
    
    2. 버튼에 들어갈 속성의 타입을 enum으로 정의했습니다.
    
    - 각 속성에 들어갈 값을 enum으로 정의하고, 주석으로 설명을 덧붙였습니다.

---
+++ React/src/types/IButtonTypes.ts
@@ -0,0 +1,88 @@
+/**
+ * @interface IButtonProps 버튼 컴포넌트 props 타입 정의
+ * - btnText: 버튼 내부에 들어갈 텍스트
+ * - btnTextSize: 버튼 텍스트 크기
+ * - btnWidth: 버튼 가로 길이
+ * - btnHeight: 버튼 세로 길이
+ * - btnRadius: 버튼 border-radius
+ * - btnColor: 버튼 배경색
+ */
+export interface IButtonProps {
+  btnText: string;
+  btnTextSize: BtnTextSize;
+  btnWidth: BtnWidth;
+  btnHeight: BtnHeight;
+  btnRadius: BtnRadius;
+  btnColor: BtnColor;
+}
+
+/**
+ * @param BtnTextSize 버튼 텍스트 크기
+ * - 사용방법 예시: BtnTextSize.Large
+ * - L, M, MS => Large
+ * - S => Small
+ * - Large: 'text-[14px]'
+ * - Small: 'text-[12px]'
+ */
+export enum BtnTextSize {
+  Large = 'text-[14px]',
+  Small = 'text-[12px]',
+}
+
+/**
+ * @param BtnWidth 버튼 가로 길이
+ * - 사용방법 예시: BtnWidth.Large
+ * - Large: 'w-[322px]'
+ * - Medium: 'w-[120px]'
+ * - MediumSmall: 'w-[90px]'
+ * - Small: 'w-[56px]'
+ */
+export enum BtnWidth {
+  Large = 'w-[322px]',
+  Medium = 'w-[120px]',
+  MediumSmall = 'w-[90px]',
+  Small = 'w-[56px]',
+}
+
+/**
+ * @param BtnHeight 버튼 세로 길이
+ * - 사용방법 예시: BtnHeight.Large
+ * - Large: 'h-[44px]'
+ * - Medium: 'h-[34px]'
+ * - MediumSmall: 'h-[32px]'
+ * - Small: 'h-[28px]'
+ */
+export enum BtnHeight {
+  Large = 'h-[44px]',
+  Medium = 'h-[34px]',
+  MediumSmall = 'h-[32px]',
+  Small = 'h-[28px]',
+}
+
+/**
+ * @param BtnRadius 버튼 border-radius
+ * - 사용방법 예시: BtnRadius.Large
+ * - Large: 'rounded-[44px]'
+ * - Medium: 'rounded-[30px]'
+ * - MediumSmall: 'rounded-[32px]'
+ * - Small: 'rounded-[26px]'
+ */
+export enum BtnRadius {
+  Large = 'rounded-[44px]',
+  Medium = 'rounded-[30px]',
+  MediumSmall = 'rounded-[32px]',
+  Small = 'rounded-[26px]',
+}
+
+/**
+ * @param BtnColor 버튼 배경색
+ * - 사용방법 예시: BtnColor.Active
+ * - Active: 'bg-[#F26E22]'
+ * - Inactive: 'bg-[#FFC7A7]'
+ * - Disabled: 'bg-[#DBDBDB]'
+ */
+export enum BtnColor {
+  Active = 'bg-[#F26E22]',
+  Inactive = 'bg-[#FFC7A7]',
+  Disabled = 'bg-[#DBDBDB]',
+}
