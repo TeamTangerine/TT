542483fecbd873ffd613db8516af9706b64895a9
Liebe_Chaeso <d131215@naver.com>
2025-09-06 10:36:28 +0900

    fix/button 타입이름 변경 enum 으로 정의한 타입에 접두사 'I' 추가

--- React/src/types/IButtonTypes.ts
+++ React/src/types/IButtonTypes.ts
@@ -1,7 +1,7 @@
 /**
  * @interface IButtonProps 버튼 컴포넌트 props 타입 정의
  * - btnText: 버튼 내부에 들어갈 텍스트
- * - btnTextSize: 버튼 텍스트 크기
+ * - btnTextSize: 버튼 텍스트 크기:
  * - btnWidth: 버튼 가로 길이
  * - btnHeight: 버튼 세로 길이
  * - btnRadius: 버튼 border-radius
@@ -9,35 +9,35 @@
  */
 export interface IButtonProps {
   btnText: string;
-  btnTextSize: BtnTextSize;
-  btnWidth: BtnWidth;
-  btnHeight: BtnHeight;
-  btnRadius: BtnRadius;
-  btnColor: BtnColor;
+  btnTextSize: IBtnTextSize;
+  btnWidth: IBtnWidth;
+  btnHeight: IBtnHeight;
+  btnRadius: IBtnRadius;
+  btnColor: IBtnColor;
 }
 
 /**
- * @param BtnTextSize 버튼 텍스트 크기
+ * @param IBtnTextSize 버튼 텍스트 크기
  * - 사용방법 예시: BtnTextSize.Large
  * - L, M, MS => Large
  * - S => Small
  * - Large: 'text-[14px]'
  * - Small: 'text-[12px]'
  */
-export enum BtnTextSize {
+export enum IBtnTextSize {
   Large = 'text-[14px]',
   Small = 'text-[12px]',
 }
 
 /**
- * @param BtnWidth 버튼 가로 길이
+ * @param IBtnWidth 버튼 가로 길이
  * - 사용방법 예시: BtnWidth.Large
  * - Large: 'w-[322px]'
  * - Medium: 'w-[120px]'
  * - MediumSmall: 'w-[90px]'
  * - Small: 'w-[56px]'
  */
-export enum BtnWidth {
+export enum IBtnWidth {
   Large = 'w-[322px]',
   Medium = 'w-[120px]',
   MediumSmall = 'w-[90px]',
@@ -45,14 +45,14 @@ export enum BtnWidth {
 }
 
 /**
- * @param BtnHeight 버튼 세로 길이
+ * @param IBtnHeight 버튼 세로 길이
  * - 사용방법 예시: BtnHeight.Large
  * - Large: 'h-[44px]'
  * - Medium: 'h-[34px]'
  * - MediumSmall: 'h-[32px]'
  * - Small: 'h-[28px]'
  */
-export enum BtnHeight {
+export enum IBtnHeight {
   Large = 'h-[44px]',
   Medium = 'h-[34px]',
   MediumSmall = 'h-[32px]',
@@ -60,14 +60,14 @@ export enum BtnHeight {
 }
 
 /**
- * @param BtnRadius 버튼 border-radius
+ * @param IBtnRadius 버튼 border-radius
  * - 사용방법 예시: BtnRadius.Large
  * - Large: 'rounded-[44px]'
  * - Medium: 'rounded-[30px]'
  * - MediumSmall: 'rounded-[32px]'
  * - Small: 'rounded-[26px]'
  */
-export enum BtnRadius {
+export enum IBtnRadius {
   Large = 'rounded-[44px]',
   Medium = 'rounded-[30px]',
   MediumSmall = 'rounded-[32px]',
@@ -75,13 +75,13 @@ export enum BtnRadius {
 }
 
 /**
- * @param BtnColor 버튼 배경색
+ * @param IBtnColor 버튼 배경색
  * - 사용방법 예시: BtnColor.Active
  * - Active: 'bg-[#F26E22]'
  * - Inactive: 'bg-[#FFC7A7]'
  * - Disabled: 'bg-[#DBDBDB]'
  */
-export enum BtnColor {
+export enum IBtnColor {
   Active = 'bg-[#F26E22]',
   Inactive = 'bg-[#FFC7A7]',
   Disabled = 'bg-[#DBDBDB]',
