a50d940f8ddc2fab1d76e532cb31d70e9a0cca78
MinKyeonghyeon <28599@naver.com>
2025-09-08 10:10:31 +0900

    feat/유저 프로필 타입 정의하는 인터페이스 작성

---
+++ React/src/types/IUserProfileProps.ts
@@ -0,0 +1,15 @@
+export interface UserProfileProps {
+  // 컴포넌트에서 사용하는 속성
+  image: string;
+  username: string;
+  accountname: string;
+
+  // 옵셔널 속성
+  _id?: string;
+  isfollow?: boolean;
+  following?: string[];
+  follower?: string[];
+  followerCount?: number;
+  followingCount?: number;
+  actionButton?: React.ReactNode;
+}
