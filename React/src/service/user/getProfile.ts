7f5b2a2733d07968bf8140342330542a82e1c47a
Liebe_Chaeso <d131215@naver.com>
2025-09-09 17:58:59 +0900

    feat/service-user 프로필 정보 불러오기 getProfile함수 작업

---
+++ React/src/service/user/getProfile.ts
@@ -0,0 +1,39 @@
+/**
+ * 응답예시
+ * // SUCCESS
+ * -
+ * -	{
+ * -	    "user": {
+ * -	        "_id": String,
+ * -	        "username": String,
+ * -	        "accountname": String,
+ * -	        "image": String,
+ * -	        "isfollow": false,
+ * -	        "following": [],
+ * -	        "follower": [],
+ * -	        "followerCount": 0,
+ * -	        "followingCount": 0
+ * -	    }
+ * -	}
+ */
+export default async function getProfile(token: string) {
+  const url = 'https://dev.wenivops.co.kr/services/mandarin';
+
+  try {
+    const res = await fetch(url + '/user/myinfo', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error('통신 에러 발생');
+    }
+
+    const resJson = await res.json();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
