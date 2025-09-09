32432e5b6a49521a4b8666cf259b735ab63a639b
Liebe_Chaeso <d131215@naver.com>
2025-09-09 19:22:52 +0900

    feat/membership-profile 마크업 및 디자인 작업 완료

---
+++ React/src/pages/Auth/MebershipProfile.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import basicProfileImg from '../../assets/basic-profile-img.png';
+import ImgBtn from '../../assets/upload-file.png';
+import TextInput from '../../components/TextInput';
+import Button from '../../components/Button/Button';
+
+export default function MebershipProfile() {
+  return (
+    <>
+      <main>
+        <div className="my-[30px] flex flex-col items-center relative">
+          <img className="w-[110px] h-[110px] rounded-full " src={basicProfileImg} alt="내 프로필 이미지" />
+          <button className="absolute bottom-0 translate-x-[37px]">
+            <img className="w-9 h-9 rounded-full " src={ImgBtn} alt="프로필 이미지 변경 버튼" />
+          </button>
+        </div>
+        <form className="flex flex-col items-center gap-[16px] px-[34px]">
+          <TextInput inputId="userName" labelText="사용자 이름" placeholderText="2~10자 이내여야 합니다." />
+          <TextInput
+            inputId="accountId"
+            labelText="계정 ID"
+            placeholderText="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
+          />
+          <TextInput inputId="userName" labelText="소개" placeholderText="자신과 판매할 상품에 대해 소개해 주세요!" />
+          <div className="mt-[14px]">
+            <Button btnTextContent="감귤마켓 시작하기" btnSize="large" btnColor="disable" btnType="submit" />
+          </div>
+        </form>
+      </main>
+    </>
+  );
+}
