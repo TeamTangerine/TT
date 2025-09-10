9500a4007d26aa2cacc8507da50ef3b43cff026f
MinKyeongHyeon <28599@naver.com>
2025-09-10 17:13:22 +0900

    feat/Product validation, userID validation추가

--- React/src/Utils/validation.ts
+++ React/src/Utils/validation.ts
@@ -1,9 +1,15 @@
-//form validation 함수입니다
+//form UserValidation 함수입니다
 
-type Validation = {
+type UserValidation = {
+  id: string;
   userName: string;
   password: string;
   email: string;
+};
+type ProductValidation = {
+  productName: string;
+  productPrice: string;
+  productURL: string;
 };
 
 /**
@@ -11,7 +17,7 @@ type Validation = {
  * @param email - ***@***.*** 이메일 기본 형식
  * @returns {boolean}
  */
-function validateEmail({ email }: Validation) {
+function validateEmail({ email }: UserValidation) {
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   let isOk = emailRegex.test(email);
   return isOk;
@@ -23,21 +29,71 @@ function validateEmail({ email }: Validation) {
  * @returns {boolean}
  */
 
-function validateUserName({ userName }: Validation) {
+function validateUserName({ userName }: UserValidation) {
   const idRegex = /^[a-zA-Z0-9가-힣]+( [a-zA-Z0-9가-힣]+)*$/;
   let isOk = idRegex.test(userName) && userName.length >= 2 && userName.length <= 10;
   return isOk;
 }
-
+/**
+ * 계정 ID 검증 함수
+ * @param id - 1자 이상의 영어 대소문자, 특수문자(.),(_)
+ * @returns {boolean}
+ */
+function validateId({ id }: UserValidation) {
+  const isRegex = /^[a-zA-Z._]{1,}$/;
+  let isOk = isRegex.test(id);
+  return isOk;
+}
 /**
  * 비밀번호 검증 함수
  * @param {string} password - 6자 이상이어야 하며, 영문 대소문자, 숫자, 특수문자를 모두 포함
  * @returns {boolean}
  */
 
-function validatePassword({ password }: Validation) {
+function validatePassword({ password }: UserValidation) {
   const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
   let isOk = passwordRegex.test(password);
   return isOk;
 }
-export { validateEmail, validateUserName, validatePassword };
+
+//=============상품 등록용 validate 함수 ==============
+/**
+ * 상품명 검증 함수
+ * @param productName - 2자 ~ 15자 이내의 영문 대소문자, 한글 및 숫자 (단어사이 공백 허용, 연속된 공백이나 단어 시작 끝은 공백 불허)
+ * @returns {boolean}
+ */
+function validateProductName({ productName }: ProductValidation) {
+  const productNameRegex = /^[a-zA-Z0-9가-힣]+( [a-zA-Z0-9가-힣]+)*$/;
+  let isOk = productNameRegex.test(productName) && productName.length >= 2 && productName.length <= 15;
+  return isOk;
+}
+/**
+ * 가격이 숫자인지 검증 함수
+ * @param productPrice - 숫자 형태의 '문자열'만 가능(혹시 몰라서 특수기호(,) 추가)
+ * @returns {boolean}
+ */
+function validateProductPrice({ productPrice }: ProductValidation) {
+  const productPriceRegex = /^[0-9,]$/;
+  let isOk = productPriceRegex.test(productPrice);
+  return isOk;
+}
+/**
+ * 제품 URL을 검증 함수
+ * @param param0 -URL형태 https://***.***.***
+ *  @returns {boolean}
+ */
+function validateProductURL({ productURL }: ProductValidation) {
+  const productPriceRegex = /^(https?|ftp):\/\/(-\.)?([^\s\/?\.#-]+\.?)+(\/[^\s]*)?$/i;
+  let isOk = productPriceRegex.test(productURL);
+  return isOk;
+}
+
+export {
+  validateEmail,
+  validateUserName,
+  validatePassword,
+  validateId,
+  validateProductName,
+  validateProductPrice,
+  validateProductURL,
+};
