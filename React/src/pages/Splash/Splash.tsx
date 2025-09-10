005dbdec2a0bd3556ad3b52127d7dc8452085bcb
Liebe_Chaeso <d131215@naver.com>
2025-09-10 20:35:39 +0900

    feat/Splash Splash 기능 구현
    -Splash 페이지에서 2초간 머무른 후 다른 페이지로 이동합니다.

--- React/src/pages/Splash/Splash.tsx
+++ React/src/pages/Splash/Splash.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { useState } from 'react';
+
+import Login from '../Auth/Login';
 import fullLogo from '../../assets/full-logo.png';
 
-export default function Splash() {
+function Splash() {
+  const [showSplash, setShowSplash] = useState('opacity-100');
+
+  if (showSplash === 'opacity-100') {
+    const timer = setTimeout(() => {
+      setShowSplash('hidden');
+      clearTimeout(timer);
+    }, 2000);
+  }
+
+  // Login 되지 않은 상태의 경우 - <Login /> 으로 이동
+  // Login 된 상태의 경우 - <Home /> 으로 이동
+  if (showSplash === 'hidden') {
+    return <Login />;
+  }
+
   return (
-    <div className="flex justify-center content-center items-center h-screen">
-      <img src={fullLogo} className="width" alt="감귤마켓로고" />
+    <div>
+      <div
+        className={`absolute z-30 flex justify-center content-center items-center w-screen h-screen bg-white ${showSplash}`}
+      >
+        <img src={fullLogo} className="width" alt="감귤마켓로고" />
+      </div>
     </div>
   );
 }
+
+export default Splash;
