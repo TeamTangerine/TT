c2651f25b3192ee5a5de369550fc434718bf6db9
Liebe_Chaeso <d131215@naver.com>
2025-09-11 11:34:51 +0900

    refactor/splash splash 기능 리팩터
    - 로고 바운스 애니메이션 추가
    - useEffect를 통해 setTimeout 함수 관리

--- React/src/pages/Splash/Splash.tsx
+++ React/src/pages/Splash/Splash.tsx
@@ -1,33 +1,40 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Login from '../Auth/Login';
 import fullLogo from '../../assets/full-logo.png';
+import logoImage from '../../assets/logo-image.png';
+import logoText from '../../assets/logo-text.png';
 
 function Splash() {
   const [showSplash, setShowSplash] = useState('opacity-100');
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  if (showSplash === 'opacity-100') {
+  useEffect(() => {
     const timer = setTimeout(() => {
       setShowSplash('hidden');
+    }, 3000);
+
+    return () => {
       clearTimeout(timer);
-    }, 2000);
-  }
+    };
+  }, [showSplash]);
 
   // Login 되지 않은 상태의 경우 - <Login /> 으로 이동
   // Login 된 상태의 경우 - <Home /> 으로 이동
   if (showSplash === 'hidden') {
-    navigate('/login');
+    // navigate('/login');
+    return <Login />;
   }
 
   return (
     <div>
       <div
-        className={`absolute z-30 flex justify-center content-center items-center w-screen h-screen bg-white ${showSplash}`}
+        className={`absolute z-30 flex flex-col justify-center content-center items-center gap-[17.57px] w-screen h-screen bg-white ${showSplash}`}
       >
-        <img src={fullLogo} className="width" alt="감귤마켓로고" />
+        <img src={logoImage} className="animate-bounce" alt="감귤마켓로고" />
+        <img src={logoText} alt="감귤마켓" />
       </div>
     </div>
   );
