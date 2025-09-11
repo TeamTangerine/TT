9d7f92d93bbb99c4c2e832f1045fb13df1987145
gyeone <voov000@naver.com>
2025-09-11 12:15:29 +0900

    feat/chatList 페이지 이동 기능 추가

--- React/src/pages/chat/ChatList.tsx
+++ React/src/pages/chat/ChatList.tsx
@@ -1,50 +1,55 @@
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/Header';
 import profileImg from '../../assets/basic-profile-img.png';
+import { Link } from 'react-router-dom';
 
 function ChatList() {
   return (
     <>
       <Header />
       <ul className="flex flex-col gap-5 pt-6 px-[16px]">
-        <li className="flex gap-3 items-end w-full justify-between">
-          <div className="relative">
-            <span className="w-3 h-3 bg-[#F26E22] rounded-full absolute top-0"></span>
+        <Link to="/chat-room">
+          <li className="flex gap-3 items-end w-full justify-between">
+            <div className="relative">
+              <span className="w-3 h-3 bg-[#F26E22] rounded-full absolute top-0"></span>
+              <img src={profileImg} alt="채팅 상대방 프로필 이미지" className="w-[42px] h-[42px] rounded-full" />
+            </div>
+
+            <div className="flex flex-col gap-1 flex-1 ">
+              <h2 className="text-sm font-medium">애월읍 위니브 감귤농장</h2>
+              <p className="text-[12px] text-[#767676] mb-[3px]">이번에 정정 언제하맨마씸?</p>
+            </div>
+            <time className="text-[10px] text-[#DBDBDB] ml-[1px]">2020.10.25</time>
+          </li>
+        </Link>
+        <Link to="/chat-room">
+          <li className="flex gap-3 items-end w-full justify-between">
+            <div className="relative">
+              <span className="w-3 h-3 bg-[#F26E22] rounded-full absolute top-0"></span>
+              <img src={profileImg} alt="채팅 상대방 프로필 이미지" className="w-[42px] h-[42px] rounded-full" />
+            </div>
+
+            <div className="flex flex-col gap-1 flex-1 min-w-0">
+              <h2 className="text-sm font-medium">제주감귤마을</h2>
+              <p className="text-[12px] text-[#767676] mb-[3px] truncate">
+                깊은 어둠의 존재감, 롤스로이스 뉴 블랙 배지...
+              </p>
+            </div>
+            <time className="text-[10px] text-[#DBDBDB] ml-[1px]">2020.10.25</time>
+          </li>
+        </Link>
+        <Link to="/chat-room">
+          <li className="flex gap-3 items-end w-full justify-between">
             <img src={profileImg} alt="채팅 상대방 프로필 이미지" className="w-[42px] h-[42px] rounded-full" />
-          </div>
-
-          <div className="flex flex-col gap-1 flex-1 ">
-            <h2 className="text-sm font-medium">애월읍 위니브 감귤농장</h2>
-            <p className="text-[12px] text-[#767676] mb-[3px]">이번에 정정 언제하맨마씸?</p>
-          </div>
-          <time className="text-[10px] text-[#DBDBDB] ml-[1px]">2020.10.25</time>
-        </li>
-
-        <li className="flex gap-3 items-end w-full justify-between">
-          <div className="relative">
-            <span className="w-3 h-3 bg-[#F26E22] rounded-full absolute top-0"></span>
-            <img src={profileImg} alt="채팅 상대방 프로필 이미지" className="w-[42px] h-[42px] rounded-full" />
-          </div>
-
-          <div className="flex flex-col gap-1 flex-1 min-w-0">
-            <h2 className="text-sm font-medium">제주감귤마을</h2>
-            <p className="text-[12px] text-[#767676] mb-[3px] truncate">
-              깊은 어둠의 존재감, 롤스로이스 뉴 블랙 배지...
-            </p>
-          </div>
-          <time className="text-[10px] text-[#DBDBDB] ml-[1px]">2020.10.25</time>
-        </li>
-
-        <li className="flex gap-3 items-end w-full justify-between">
-          <img src={profileImg} alt="채팅 상대방 프로필 이미지" className="w-[42px] h-[42px] rounded-full" />
-          <div className="flex flex-col gap-1 flex-1 min-w-0">
-            <h2 className="text-sm font-medium">누구네 농장 친환경 한라봉</h2>
-            <p className="text-[12px] text-[#767676] mb-[3px] truncate">
-              내 차는 내가 평가한다. 오픈 이벤트에 참여 하...
-            </p>
-          </div>
-          <time className="text-[10px] text-[#DBDBDB] ml-[1px]">2020.10.25</time>
-        </li>
+            <div className="flex flex-col gap-1 flex-1 min-w-0">
+              <h2 className="text-sm font-medium">누구네 농장 친환경 한라봉</h2>
+              <p className="text-[12px] text-[#767676] mb-[3px] truncate">
+                내 차는 내가 평가한다. 오픈 이벤트에 참여 하...
+              </p>
+            </div>
+            <time className="text-[10px] text-[#DBDBDB] ml-[1px]">2020.10.25</time>
+          </li>
+        </Link>
       </ul>
       <Footer />
     </>
