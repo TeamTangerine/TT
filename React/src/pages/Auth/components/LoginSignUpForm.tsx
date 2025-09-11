547b07051f1e57fd088e57f9aa1fbffe966a3cc9
Liebe_Chaeso <d131215@naver.com>
2025-09-11 17:16:07 +0900

    feat/login 이메일과 비밀번호 입력 시 버튼활성화

--- React/src/pages/Auth/components/LoginSignUpForm.tsx
+++ React/src/pages/Auth/components/LoginSignUpForm.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useState } from 'react';
+import Button from '../../../components/Button/Button';
+import TextInput from '../../../components/TextInput';
+import postLogin from '../../../service/user/postLogin';
 
 type LoginSignUpFormProps = {
   formName: string;
@@ -6,55 +10,45 @@ type LoginSignUpFormProps = {
 };
 
 export default function LoginSignUpForm({ formName, btnText }: LoginSignUpFormProps) {
-  let isTrue = false;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <section className="flex flex-col items-center">
       <h1 className="text-[24px] font-medium mt-[30px]">{formName}</h1>
       <div className="w-screen mt-[40px]">
         <form className="flex flex-col gap-[30px]">
-          <div className="flex flex-col gap-[16px]">
-            <div className="flex flex-col px-[34px]">
-              <label htmlFor="email" className="text-[12px] text-[#767676]">
-                이메일
-              </label>
-              <input
-                id="email"
-                type="email"
-                className="border-[#DBDBDB] border-b-2 focus:outline-none focus:border-b-1 focus:border-[#F26E22] text-[#000000]"
-              />
-
-              {/* span 태그 렌더링 기능 조건부 구현으로 구성할지 고민중 */}
-              {/* <span className="text-[12px] text-[#EB5757] mt-[6px]">*이미 가입된 이메일 주소입니다.</span> */}
-            </div>
-
-            <div className="flex flex-col px-[34px]">
-              <label htmlFor="password" className="text-[12px] text-[#767676]">
-                비밀번호
-              </label>
-              <input
-                id="password"
-                type="password"
-                className="border-[#DBDBDB] border-b-2 focus:outline-none focus:border-b-1 focus:border-[#F26E22] text-[#000000]"
-              />
-
-              {/* span 태그 렌더링 기능 조건부 구현으로 구성할지 고민중 */}
-              {/* <span className="text-[12px] text-[#EB5757] mt-[6px]">*이메일 또는 비밀번호가 일치하지 않습니다.</span> */}
-            </div>
+          <div className="flex flex-col items-center gap-[16px]">
+            <TextInput
+              inputId="email"
+              labelText="이메일"
+              inputType="email"
+              onChange={(value) => {
+                if (typeof value === 'string') {
+                  setEmail(value);
+                }
+              }}
+            />
+            <TextInput
+              inputId="password"
+              labelText="비밀번호"
+              inputType="password"
+              onChange={(value) => {
+                if (typeof value === 'string') {
+                  setPassword(value);
+                }
+              }}
+            />
           </div>
 
-          {/* 
-          버튼활성화 관련 기능 구현
-          {isTrue ? 'bg-[#F26E22]' : 'bg-[#FFC7A7]'}
-          버튼 클래스 안 해당 조건문 넣은 이유:
-          버튼이 활성화 되었을때 디자인을 변경하기 위해 미리 디자인.
-          */}
-          <button
-            type="button"
-            className={`block bg-[#FFC7A7] rounded-[44px] mx-[34px] h-[44px] py-[14px] text-[14px] font-medium text-white ${isTrue ? 'bg-[#F26E22]' : 'bg-[#FFC7A7]'}`}
-          >
-            {btnText}
-          </button>
+          <div className="flex justify-center">
+            <Button
+              btnTextContent="로그인"
+              btnSize="large"
+              btnColor={email && password ? 'normal' : 'disable'}
+              btnType="submit"
+            />
+          </div>
         </form>
       </div>
     </section>
