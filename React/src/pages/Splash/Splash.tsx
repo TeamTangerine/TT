5b20746deeebe53cf791790f3a8fb417085544a7
Liebe_Chaeso <d131215@naver.com>
2025-09-12 17:04:40 +0900

    refactor/splash 스플레쉬 기능 수정
    showSplash 상태관리 빈 값으로 수정.

--- React/src/pages/Splash/Splash.tsx
+++ React/src/pages/Splash/Splash.tsx
@@ -8,8 +8,8 @@ import logoImage from '../../assets/logo-image.png';
 import logoText from '../../assets/logo-text.png';
 
 function Splash() {
-  const [showSplash, setShowSplash] = useState('opacity-100');
-  // const navigate = useNavigate();
+  const [showSplash, setShowSplash] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -25,7 +25,7 @@ function Splash() {
   // Login 된 상태의 경우 - <Home /> 으로 이동
   if (showSplash === 'hidden') {
     // navigate('/login');
-    return <Login />;
+    return navigate('/home');
   }
 
   return (
