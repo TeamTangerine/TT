13bff3f1ccbed5baa9315b4c0c845e7bf868d65f
MinKyeongHyeon <28599@naver.com>
2025-09-13 16:25:47 +0900

    feat/backdrop기능 추가/ 전체를 빈태그로 묶고 div에 백드롭 스타일, 온클릭 이벤트 적용

--- React/src/components/modal/Modal.tsx
+++ React/src/components/modal/Modal.tsx
@@ -28,28 +28,32 @@ function Modal({ closeModal }: IModalProps) {
     }, 300);
   };
   return createPortal(
-    <dialog
-      ref={dialogRef}
-      open={isOpen}
-      className={`fixed bottom-0 flex flex-col items-center w-full max-w-[390px] rounded-t-[10px] bg-white shadow-inner
+    <>
+      {/* 백드롭(배경어둡게)용 div */}
+      <div className="fixed inset-0 bg-black bg-opacity-50 z-10" onClick={handleClose} />
+      <dialog
+        ref={dialogRef}
+        open={isOpen}
+        className={`fixed bottom-0 z-20 flex flex-col items-center w-full max-w-[390px] rounded-t-[10px] bg-white shadow-inner
     transition-transform duration-300 ${isOpen ? 'translate-y-0' : 'translate-y-full'}`}
-    >
-      <button type="button" onClick={handleClose}>
-        <img className="w-[50px] h-1 my-4" src={modalBarImg} alt="모달 바" />
-      </button>
-      <ul className="flex flex-col w-[100%] ">
-        {/* li는 기능 작업 시 props를 받아와서 조건부 렌더링으로 switch문으로 처리하기 (switch문 => li를 컴포넌트로 안 만들어도 됨) */}
-        <li className=" ml-[26px] h-[46px] ">
-          <p className="text-[14px] text-left my-[14px] h-[18px]">설정 및 개인정보</p>
-        </li>
-        <li className=" ml-[26px] h-[46px] ">
-          <p className="text-[14px] text-left my-[14px] h-[18px]">로그아웃</p>
-        </li>
-        <li>
-          <div className="h-[10vh]"></div>
-        </li>
-      </ul>
-    </dialog>,
+      >
+        <button type="button" onClick={handleClose}>
+          <img className="w-[50px] h-1 my-4" src={modalBarImg} alt="모달 바" />
+        </button>
+        <ul className="flex flex-col w-[100%] ">
+          {/* li는 기능 작업 시 props를 받아와서 조건부 렌더링으로 switch문으로 처리하기 (switch문 => li를 컴포넌트로 안 만들어도 됨) */}
+          <li className=" ml-[26px] h-[46px] ">
+            <p className="text-[14px] text-left my-[14px] h-[18px]">설정 및 개인정보</p>
+          </li>
+          <li className=" ml-[26px] h-[46px] ">
+            <p className="text-[14px] text-left my-[14px] h-[18px]">로그아웃</p>
+          </li>
+          <li>
+            <div className="h-[10vh]"></div>
+          </li>
+        </ul>
+      </dialog>
+    </>,
     document.body
   );
 }
