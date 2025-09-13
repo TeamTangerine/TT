99d883912d74ca78b4af2dd512b2db336541a82a
MinKyeongHyeon <28599@naver.com>
2025-09-13 19:13:31 +0900

    refactor/버튼을 Link태그로 교체하고 address prop과 타입 추가

--- React/src/components/Footer/component/NavButton.tsx
+++ React/src/components/Footer/component/NavButton.tsx
@@ -1,14 +1,15 @@
 type NavButtonProps = {
   imgSrc: string;
   name: string;
+  address: string;
 };
 
-function NavButton({ imgSrc, name }: NavButtonProps) {
+function NavButton({ imgSrc, name, address }: NavButtonProps) {
   return (
-    <button className="w-[84px] h-[60px] flex flex-col items-center gap-1">
+    <link to={address} className="w-[84px] h-[60px] flex flex-col items-center gap-1">
       <img src={imgSrc} alt={name} className="w-6 h-6 mt-3" />
       <span className="text-[10px] text-[#767676]">{name}</span>
-    </button>
+    </link>
   );
 }
 
