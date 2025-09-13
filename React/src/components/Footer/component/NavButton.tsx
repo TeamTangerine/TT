71c0e1f3806da66728ab19669c3d19d74ab758e0
MinKyeongHyeon <28599@naver.com>
2025-09-13 19:17:11 +0900

    style/Link태그 소문자인 부분 수정, 라우터 돔 import

--- React/src/components/Footer/component/NavButton.tsx
+++ React/src/components/Footer/component/NavButton.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 type NavButtonProps = {
   imgSrc: string;
   name: string;
@@ -6,10 +7,10 @@ type NavButtonProps = {
 
 function NavButton({ imgSrc, name, address }: NavButtonProps) {
   return (
-    <link to={address} className="w-[84px] h-[60px] flex flex-col items-center gap-1">
+    <Link to={address} className="w-[84px] h-[60px] flex flex-col items-center gap-1">
       <img src={imgSrc} alt={name} className="w-6 h-6 mt-3" />
       <span className="text-[10px] text-[#767676]">{name}</span>
-    </link>
+    </Link>
   );
 }
 
