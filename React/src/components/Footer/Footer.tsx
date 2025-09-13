7edb37c4f570e3934d2b821980ec684da192ca54
MinKyeongHyeon <28599@naver.com>
2025-09-13 19:18:06 +0900

    fix/chat-list 오타 수정

--- React/src/components/Footer/Footer.tsx
+++ React/src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
   return (
     <nav className="fixed bottom-0 flex justify-center gap-[14px] w-full h-[60px] border-t border-t-[#DBDBDB] bg-white">
       <NavButton imgSrc={iconHome} name="홈" address="/" />
-      <NavButton imgSrc={iconMessageCircle} name="채팅" address="/chat=list" />
+      <NavButton imgSrc={iconMessageCircle} name="채팅" address="/chat-list" />
       <NavButton imgSrc={iconEdit} name="게시물 작성" address="/upload" />
       <NavButton imgSrc={iconUser} name="프로필" address="/my-profile" />
     </nav>
