57fe748e3e20c52b7bdf5eff8cd04966399daade
MinKyeongHyeon <28599@naver.com>
2025-09-13 19:13:56 +0900

    refactor/버튼을 실제로 라우팅할 수 있게 기능구현

--- React/src/components/Footer/Footer.tsx
+++ React/src/components/Footer/Footer.tsx
@@ -7,10 +7,10 @@ import NavButton from './component/NavButton';
 function Footer() {
   return (
     <nav className="fixed bottom-0 flex justify-center gap-[14px] w-full h-[60px] border-t border-t-[#DBDBDB] bg-white">
-      <NavButton imgSrc={iconHome} name="홈" />
-      <NavButton imgSrc={iconMessageCircle} name="채팅" />
-      <NavButton imgSrc={iconEdit} name="게시물 작성" />
-      <NavButton imgSrc={iconUser} name="프로필" />
+      <NavButton imgSrc={iconHome} name="홈" address="/" />
+      <NavButton imgSrc={iconMessageCircle} name="채팅" address="/chat=list" />
+      <NavButton imgSrc={iconEdit} name="게시물 작성" address="/upload" />
+      <NavButton imgSrc={iconUser} name="프로필" address="/my-profile" />
     </nav>
   );
 }
