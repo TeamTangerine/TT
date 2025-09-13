2492f62cdc6c41dffaca2fcddb8058c894ecad8c
MinKyeongHyeon <28599@naver.com>
2025-09-13 23:23:05 +0900

    feat/액티브 이미지 추가/ 프롭으로 전달

--- React/src/components/Footer/Footer.tsx
+++ React/src/components/Footer/Footer.tsx
@@ -1,16 +1,19 @@
-import iconHome from '../../assets/icon/icon-home.svg'; // 경로 수정
+import iconHome from '../../assets/icon/icon-home.svg';
+import iconHomeActive from '../../assets/icon/icon-home-fill.svg';
 import iconMessageCircle from '../../assets/icon/icon-message-circle.svg';
+import iconMessageCircleActive from '../../assets/icon/icon-message-circle-fill.png';
 import iconEdit from '../../assets/icon/icon-edit.svg';
 import iconUser from '../../assets/icon/icon-user.svg';
+import iconUserActive from '../../assets/icon/icon-user-fill.png';
 import NavButton from './component/NavButton';
 
 function Footer() {
   return (
     <nav className="fixed bottom-0 flex justify-center gap-[14px] w-full h-[60px] border-t border-t-[#DBDBDB] bg-white">
-      <NavButton imgSrc={iconHome} name="홈" address="/" />
-      <NavButton imgSrc={iconMessageCircle} name="채팅" address="/chat-list" />
+      <NavButton imgSrc={iconHome} activeImgSrc={iconHomeActive} name="홈" address="/" />
+      <NavButton imgSrc={iconMessageCircle} activeImgSrc={iconMessageCircleActive} name="채팅" address="/chat-list" />
       <NavButton imgSrc={iconEdit} name="게시물 작성" address="/upload" />
-      <NavButton imgSrc={iconUser} name="프로필" address="/my-profile" />
+      <NavButton imgSrc={iconUser} activeImgSrc={iconUserActive} name="프로필" address="/my-profile" />
     </nav>
   );
 }
