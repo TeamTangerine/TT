c0f18ad0dce8467f5671b50dbd19d5ed910c376a
MinKyeongHyeon <28599@naver.com>
2025-09-13 13:07:16 +0900

    feat/ 열고 닫을때 애니메이션을 추가, 약간의 상태 관리를 추가

--- React/src/components/modal/Modal.tsx
+++ React/src/components/modal/Modal.tsx
@@ -1,13 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import modalBarImg from '../../assets/modal-bar.png';
 import { createPortal } from 'react-dom';
 interface IModalProps {
   closeModal: () => void;
 }
+/**
+ * 모달 컴포넌트
+ * @param closeModal - setShowModal(false)를 콜백으로 받음
+ * @returns
+ */
 function Modal({ closeModal }: IModalProps) {
+  //모달 열렸는지 상태
+  const [isOpen, setIsOpen] = useState(false);
+  //settimeout을 안하면 바로 올라와서 지연시킴. 렌더링시 Open=true
+  useEffect(() => {
+    setTimeout(() => {
+      setIsOpen(true);
+    }, 10);
+  }, []);
+  //버튼 온클릭 이벤트 핸들러  setOpen(false)로 애니메이션(밑에 duration-300)이 지나고 닫히도록 딜레이함.
+  const handleClose = () => {
+    setIsOpen(false);
+    setTimeout(() => {
+      closeModal();
+    }, 300);
+  };
   return createPortal(
-    <dialog className="fixed bottom-0 flex flex-col items-center w-full max-w-[390px] rounded-t-[10px] bg-white shadow-inner">
-      <button type="button" onClick={closeModal}>
+    <dialog
+      className={`fixed bottom-0 flex flex-col items-center w-full max-w-[390px] rounded-t-[10px] bg-white shadow-inner
+    transition-transform duration-300 ${isOpen ? 'translate-y-0' : 'translate-y-full'}`}
+    >
+      <button type="button" onClick={handleClose}>
         <img className="w-[50px] h-1 my-4" src={modalBarImg} alt="모달 바" />
       </button>
       <ul className="flex flex-col w-[100%] ">
