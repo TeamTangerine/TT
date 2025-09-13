bae22e92de793b7991c5c6ff2db04e7bf30cbc95
MinKyeongHyeon <28599@naver.com>
2025-09-14 00:49:57 +0900

    style/액티브시 글자 색 주황색으로 변경

--- React/src/components/Footer/component/NavButton.tsx
+++ React/src/components/Footer/component/NavButton.tsx
@@ -31,7 +31,7 @@ function NavButton({ imgSrc, name, address, activeImgSrc }: NavButtonProps) {
   return (
     <Link to={address} className="w-[84px] h-[60px] flex flex-col items-center gap-1">
       <img src={isActive ? activeImgSrc : imgSrc} alt={name} className="w-6 h-6 mt-3" />
-      <span className={`text-[10px] ${isActive ? 'text-[#000]' : 'text-[#767676]'}`}>{name}</span>
+      <span className={`text-[10px] ${isActive ? 'text-[#F26E22]' : 'text-[#767676]'}`}>{name}</span>
     </Link>
   );
 }
