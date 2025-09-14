e1002fbad1b1eba73fa245c393349810d31dbefe
Liebe_Chaeso <d131215@naver.com>
2025-09-14 12:53:14 +0900

    feat/profile 리스트형 앨범형 토글 및 렌더링 기능구현
    주요 작업사항
    1. 토글기능
    - useState, prop으로 버튼 선택 관리(showAlbum)
    - 토글 함수로 showAlbum 상태 변화 구현
    
    2. 렌더링
    - showAlbum의 상태에 따라 앨범형 렌더링과 리스트형 랜더링 구현

--- React/src/components/Posting.tsx
+++ React/src/components/Posting.tsx
@@ -3,46 +3,60 @@ import iconMoreVertical from '../assets/icon/s-icon-more-vertical.png';
 import postImgExample from '../assets/post-img-example.png';
 import iconHeart from '../assets/icon/icon-heart.png';
 import iconMessage from '../assets/icon/icon-message-circle.svg';
-function Posting() {
+
+// 리스트형 / 앨범형 선택을 위한 props 타입
+type PostingProps = {
+  showAlbum?: boolean;
+};
+
+function Posting({ showAlbum = true }: PostingProps) {
   const profileImg = basicProfileImg;
   const postImg = postImgExample;
   return (
-    <li className="flex gap-3 max-w-[328px]">
-      <img src={profileImg} alt="프로필" className="w-[42px] h-[42px]" />
-      <article className="flex flex-col gap-4">
-        <div className="flex justify-between">
-          <div className="flex flex-col gap-[2px]">
-            <h2 className="text-sm">애월읍 위니브 감귤농장</h2>
-            <p className="text-[12px] text-[#767676]">@weniv_Mandarin</p>
-          </div>
-          <button className="mt-1 w-[18px] h-[18px] flex items-center justify-center">
-            <img src={iconMoreVertical} alt="더보기" />
-          </button>
-        </div>
-        <p>
-          웃을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여, 뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와
-          악동하다. 대고 못할 넣는 풍부하게 뛰는 뛰노는 인생의 힘있다.
-        </p>
-        <img src={postImg} alt="게시글이미지" />
-        <div className="flex gap-4">
-          <div className="flex gap-[6px] items-center">
-            <button className="w-5 h-5">
-              <img src={iconHeart} alt="좋아요" />
-            </button>
-            <span className="text-[12px] text-[#767676]">58</span>
-          </div>
-          <div className="flex gap-[6px] items-center">
-            <button className="w-5 h-5">
-              <img src={iconMessage} alt="댓글" />
-            </button>
-            <span className="text-[12px] text-[#767676]">12</span>
-          </div>
-        </div>
-        <time dateTime="2020-10-21" className="text-[10px] text-[#767676]">
-          2020년 10월 21일
-        </time>
-      </article>
-    </li>
+    <>
+      {showAlbum ? (
+        <li className="flex gap-3 max-w-[328px]">
+          <img src={profileImg} alt="프로필" className="w-[42px] h-[42px]" />
+          <article className="flex flex-col gap-4">
+            <div className="flex justify-between">
+              <div className="flex flex-col gap-[2px]">
+                <h2 className="text-sm">애월읍 위니브 감귤농장</h2>
+                <p className="text-[12px] text-[#767676]">@weniv_Mandarin</p>
+              </div>
+              <button className="mt-1 w-[18px] h-[18px] flex items-center justify-center">
+                <img src={iconMoreVertical} alt="더보기" />
+              </button>
+            </div>
+            <p>
+              웃을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여, 뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와
+              악동하다. 대고 못할 넣는 풍부하게 뛰는 뛰노는 인생의 힘있다.
+            </p>
+            <img src={postImg} alt="게시글이미지" />
+            <div className="flex gap-4">
+              <div className="flex gap-[6px] items-center">
+                <button className="w-5 h-5">
+                  <img src={iconHeart} alt="좋아요" />
+                </button>
+                <span className="text-[12px] text-[#767676]">58</span>
+              </div>
+              <div className="flex gap-[6px] items-center">
+                <button className="w-5 h-5">
+                  <img src={iconMessage} alt="댓글" />
+                </button>
+                <span className="text-[12px] text-[#767676]">12</span>
+              </div>
+            </div>
+            <time dateTime="2020-10-21" className="text-[10px] text-[#767676]">
+              2020년 10월 21일
+            </time>
+          </article>
+        </li>
+      ) : (
+        <li className="w-full aspect-square">
+          <img src={postImg} alt="게시글 이미지" className="w-full h-full object-cover" />
+        </li>
+      )}
+    </>
   );
 }
 export default Posting;
--- React/src/components/PostingGrid.tsx
+++ React/src/components/PostingGrid.tsx
@@ -6,26 +6,54 @@ import postListOn from '../assets/icon/icon-post-list-on.png';
 import { useState } from 'react';
 
 function HomeCardGrid() {
-  const listBtn = postListOn;
-  const albumBtn = postAlbumOff;
+  const [showAlbum, setShowAlbum] = useState(true);
+  const listBtnOn = postListOn;
+  const listBtnOff = postListOff;
+  const albumBtnOn = postAlbumOn;
+  const albumBtnOff = postAlbumOff;
+
+  // 토글 상태 관리 함수
+  function toggleAlbum(value: boolean) {
+    if (value === true) {
+      setShowAlbum(false);
+    }
+
+    if (value === false) {
+      setShowAlbum(true);
+    }
+  }
 
   return (
     <section className="flex flex-col">
       <div className="flex justify-center bg-white border-b border-b-[#DBDBDB]">
         <div className="min-w-[390px] flex justify-end gap-4 px-4 py-[9px]">
           <button className="w-[26px] h-[26px]">
-            <img src={listBtn} alt="리스트로 보기" />
+            <img
+              src={showAlbum ? listBtnOn : listBtnOff}
+              alt="리스트로 보기"
+              onClick={() => {
+                toggleAlbum(false);
+              }}
+            />
           </button>
           <button className="w-[26px] h-[26px]">
-            <img src={albumBtn} alt="앨범으로 보기" />
+            <img
+              src={!showAlbum ? albumBtnOn : albumBtnOff}
+              alt="앨범으로 보기"
+              onClick={() => {
+                toggleAlbum(true);
+              }}
+            />
           </button>
         </div>
       </div>
-      <ul className="pt-6 flex flex-col items-center gap-6 px-4 bg-white">
-        <Posting />
-        <Posting />
-        <Posting />
-        <Posting />
+      <ul
+        className={`${showAlbum ? 'flex flex-col items-center gap-6' : 'grid grid-cols-3 justify-items-center gap-x-[6px] gap-y-[6px] min-h-[144px]'} pt-6  px-4 bg-white`}
+      >
+        <Posting showAlbum={showAlbum} />
+        <Posting showAlbum={showAlbum} />
+        <Posting showAlbum={showAlbum} />
+        <Posting showAlbum={showAlbum} />
       </ul>
     </section>
   );
