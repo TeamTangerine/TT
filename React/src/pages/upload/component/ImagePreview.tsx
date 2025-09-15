cd442f71b74d8def9b9fe18e1e88c1545bf533e4
MinKyeongHyeon <28599@naver.com>
2025-09-15 23:11:45 +0900

    feat/ remove이벤트 추가 파일이 삭제되도록 변경

--- React/src/pages/upload/component/ImagePreview.tsx
+++ React/src/pages/upload/component/ImagePreview.tsx
@@ -2,23 +2,32 @@ import deleteButton from '../../../assets/icon/icon-delete.svg';
 type ImagePreviewProps = {
   url: string[] | string;
   gridType: string;
+  onRemove?: (i: number) => void;
 };
 
-function ImagePreview({ url, gridType }: ImagePreviewProps) {
+function ImagePreview({ url, gridType, onRemove }: ImagePreviewProps) {
   if (gridType === 'single') {
     return (
       <div className="relative ">
         <img src={url[0]} alt="업로드 이미지" className="w-[304px] h-[228px] rounded-[10px] object-cover" />
-        <button className="absolute top-[6px] right-[6px]">
+        <button type="button" className="absolute top-[6px] right-[6px]" onClick={() => onRemove && onRemove(0)}>
           <img src={deleteButton} alt="삭제하기" />
         </button>
       </div>
     );
   }
   return (
-    <div>
-      <img src={url} alt="업로드 이미지" />
-    </div>
+    <ul className="flex flex-row gap-2 scroll-smooth snap-start overflow-x-auto">
+      {Array.isArray(url) && //타입가드
+        url.map((v, i) => (
+          <li key={i} className="relative">
+            <img src={v} alt={`업로드한 이미지${i}`} className="min-w-[168px] h-[126px] rounded-[10px] object-cover" />
+            <button type="button" className="absolute top-[6px] right-[6px]" onClick={() => onRemove && onRemove(i)}>
+              <img src={deleteButton} alt="삭제하기" />
+            </button>
+          </li>
+        ))}
+    </ul>
   );
 }
 export default ImagePreview;
