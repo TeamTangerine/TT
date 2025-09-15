18a329c5cd6087ed4145f0aa7807a75e1319b1e3
Liebe_Chaeso <d131215@naver.com>
2025-09-15 19:32:26 +0900

    feat/profile Posting컴포넌트 프롭 추가, yourProfile 페이지 게시물 랜더링
    - Posting 컴포넌트에 API로 받아올 데이터 prop으로 관리
    - API 데이터를 받아서 yourProfile 페이지 게시물 렌더링

--- React/src/components/Posting.tsx
+++ React/src/components/Posting.tsx
@@ -12,52 +12,77 @@ import iconMessage from '../assets/icon/icon-message-circle.svg';
  * @param hasPhoto
  * - 사진이 있는 경우: true
  * - 사진이 없는 경우: false
+ * @param userProfileImage -
+ * @param userName
+ * @param userId
+ * @param userContent
+ * @param contentImage
+ * @param heartCount
+ * @param commentCount
+ * @param createdAt -
  */
 type PostingProps = {
   showAlbum?: boolean;
   hasPhoto?: boolean;
+  userProfileImage?: string;
+  userName?: string;
+  userId?: string;
+  userContent?: string;
+  contentImage?: string;
+  heartCount?: number;
+  commentCount?: number;
+  updatedAt?: string;
 };
 
-function Posting({ showAlbum = true, hasPhoto = true }: PostingProps) {
+function Posting({
+  showAlbum = true,
+  hasPhoto = true,
+  userProfileImage,
+  userName,
+  userId,
+  userContent,
+  contentImage,
+  heartCount,
+  commentCount,
+  updatedAt,
+}: PostingProps) {
   const profileImg = basicProfileImg;
   const postImg = postImgExample;
+
   return (
     <>
       {showAlbum ? (
         // 리스트형 랜더링
         <li className="flex gap-3 max-w-[328px]">
-          <img src={profileImg} alt="프로필" className="w-[42px] h-[42px]" />
+          <img src={!!userProfileImage ? userProfileImage : profileImg} alt="프로필" className="w-[42px] h-[42px]" />
           <article className="flex flex-col gap-4">
             <div className="flex justify-between">
               <div className="flex flex-col gap-[2px]">
-                <h2 className="text-sm">애월읍 위니브 감귤농장</h2>
-                <p className="text-[12px] text-[#767676]">@weniv_Mandarin</p>
+                <h2 className="text-sm">{userName}</h2>
+                <p className="text-[12px] text-[#767676]">{userId}</p>
               </div>
               <button className="mt-1 w-[18px] h-[18px] flex items-center justify-center">
                 <img src={iconMoreVertical} alt="더보기" />
               </button>
             </div>
-            <p>
-              웃을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여, 뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와
-              악동하다. 대고 못할 넣는 풍부하게 뛰는 뛰노는 인생의 힘있다.
-            </p>
+            <p className="break-all whitespace-pre-wrap">{userContent}</p>
             <img src={postImg} alt="게시글이미지" />
             <div className="flex gap-4">
               <div className="flex gap-[6px] items-center">
                 <button className="w-5 h-5">
                   <img src={iconHeart} alt="좋아요" />
                 </button>
-                <span className="text-[12px] text-[#767676]">58</span>
+                <span className="text-[12px] text-[#767676]">{heartCount}</span>
               </div>
               <div className="flex gap-[6px] items-center">
                 <button className="w-5 h-5">
                   <img src={iconMessage} alt="댓글" />
                 </button>
-                <span className="text-[12px] text-[#767676]">12</span>
+                <span className="text-[12px] text-[#767676]">{commentCount}</span>
               </div>
             </div>
             <time dateTime="2020-10-21" className="text-[10px] text-[#767676]">
-              2020년 10월 21일
+              {updatedAt}
             </time>
           </article>
         </li>
--- React/src/components/PostingGrid.tsx
+++ React/src/components/PostingGrid.tsx
@@ -4,9 +4,16 @@ import postAlbumOn from '../assets/icon/icon-post-album-on.png';
 import postListOff from '../assets/icon/icon-post-list-off.png';
 import postListOn from '../assets/icon/icon-post-list-on.png';
 import { useState } from 'react';
+import { userAPI } from '../service/fetch/api';
+import { postAPI } from '../service/fetch/api';
 
 function HomeCardGrid() {
   const [showAlbum, setShowAlbum] = useState(true);
+  const [token, setToken] = useState('');
+  const [accountName, setAccountName] = useState('');
+  const [posts, setPosts] = useState<any>([]);
+  const [loading, setLoading] = useState(false);
+
   const listBtnOn = postListOn;
   const listBtnOff = postListOff;
   const albumBtnOn = postAlbumOn;
@@ -23,41 +30,97 @@ function HomeCardGrid() {
     }
   }
 
+  // 토큰 발급 함수
+  async function getTestToken() {
+    const email = 'tt1team@example.com'; // 테스트 계정 이메일
+    const password = 'test1team_'; // 테스트 계정 비밀번호
+    try {
+      const res = await userAPI.login(email, password);
+      const token = res.token;
+      const name = res.accountname;
+      console.log(token);
+      setToken(token);
+      setAccountName(name);
+    } catch (e) {
+      console.error('테스트 토큰 발급 실패:');
+      return null;
+    }
+  }
+
+  // 게시물 목록을 받아오는 함수
+  async function getUserPosts() {
+    if (!accountName || !token) {
+      console.log('토큰이나 계정명이 없습니다.');
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const postData = await postAPI.getUserPosts(accountName, token);
+      console.log('포스트 데이터: ', postData);
+      setPosts(postData.post);
+    } catch (error) {
+      console.log('포스트 조회 실패:', error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <section className="flex flex-col">
-      <div className="flex justify-center bg-white border-b border-b-[#DBDBDB]">
-        <div className="min-w-[390px] flex justify-end gap-4 px-4 py-[9px]">
-          <button className="w-[26px] h-[26px]">
-            <img
-              src={showAlbum ? listBtnOn : listBtnOff}
-              alt="리스트로 보기"
-              onClick={() => {
-                toggleAlbum(false);
-              }}
-            />
-          </button>
-          <button className="w-[26px] h-[26px]">
-            <img
-              src={!showAlbum ? albumBtnOn : albumBtnOff}
-              alt="앨범으로 보기"
-              onClick={() => {
-                toggleAlbum(true);
-              }}
-            />
-          </button>
+    <>
+      <button onClick={getTestToken}>get Token</button>
+      <button onClick={getUserPosts}>get User Posts</button>
+      <section className={`flex flex-col ${posts.length === 0 ? 'sr-only' : ''}`}>
+        <div className="flex justify-center bg-white border-b border-b-[#DBDBDB]">
+          <div className="min-w-[390px] flex justify-end gap-4 px-4 py-[9px]">
+            <button className="w-[26px] h-[26px]">
+              <img
+                src={showAlbum ? listBtnOn : listBtnOff}
+                alt="리스트로 보기"
+                onClick={() => {
+                  toggleAlbum(false);
+                }}
+              />
+            </button>
+            <button className="w-[26px] h-[26px]">
+              <img
+                src={!showAlbum ? albumBtnOn : albumBtnOff}
+                alt="앨범으로 보기"
+                onClick={() => {
+                  toggleAlbum(true);
+                }}
+              />
+            </button>
+          </div>
         </div>
-      </div>
-      <ul
-        className={`${showAlbum ? 'flex flex-col items-center gap-6' : 'grid grid-cols-3 justify-items-center gap-x-[6px] gap-y-[6px] min-h-[144px]'} pt-6  px-4 bg-white`}
-      >
-        <Posting showAlbum={showAlbum} />
-        <Posting showAlbum={showAlbum} />
-        <Posting showAlbum={showAlbum} />
-        <Posting showAlbum={showAlbum} />
-        <Posting showAlbum={showAlbum} hasPhoto={false} />
-        <Posting showAlbum={showAlbum} />
-      </ul>
-    </section>
+        <ul
+          className={`${showAlbum ? 'flex flex-col items-center gap-6' : 'grid grid-cols-3 justify-items-center gap-x-[6px] gap-y-[6px] min-h-[144px]'} pt-6  px-4 bg-white`}
+        >
+          {loading ? (
+            <li>로딩 중...</li>
+          ) : (
+            posts.map((post: any) => {
+              console.log(post);
+              return (
+                <Posting
+                  showAlbum={showAlbum}
+                  hasPhoto={true}
+                  userProfileImage={post.author.image}
+                  userName={post.author.username}
+                  userId={post.author.accountname}
+                  userContent={post.content}
+                  contentImage={''}
+                  heartCount={post.heartCount}
+                  commentCount={post.commentCount}
+                  updatedAt={post.updatedAt}
+                />
+              );
+            })
+          )}
+        </ul>
+      </section>
+    </>
   );
 }
 export default HomeCardGrid;
