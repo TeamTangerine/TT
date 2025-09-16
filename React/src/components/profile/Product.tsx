8cc725dd3e849b50b57c872b1f6e273a0a0f4439
Liebe_Chaeso <d131215@naver.com>
2025-09-16 21:24:02 +0900

    refactor/profile 상품리스트 랜더링 방식 리팩터링(토큰 추가)
    - 토큰 추가하여 API로 제대로 데이터를 받아오는지 확인
    - productLink prop추가하여 상품 판매사이트 연결

--- React/src/components/profile/Product.tsx
+++ React/src/components/profile/Product.tsx
@@ -10,19 +10,24 @@ type ProductProps = {
   itemImage: string;
   itemName: string;
   price: number;
+  productLink: string;
 };
 
-function Product({ itemImage = productImgSample, itemName, price }: ProductProps) {
+function Product({ itemImage, itemName, price, productLink }: ProductProps) {
   const [itemImg, setItemImg] = useState('');
 
-  const formattedPrice = price.toLocaleString('ko-KR');
+  const formattedPrice = price.toLocaleString();
 
-  useEffect(() => {
-    setItemImg(itemImage);
-  }, [itemImg]);
+  function handleLink() {
+    window.open(
+      'https://smartstore.naver.com/gyuldamwon/products/6474882238?nl-query=%EA%B0%90%EA%B7%A4&nl-ts-pid=jKDsylqo15wssKKQX8wsssssses-033364&NaPm=ct%3Dmfmflihk%7Cci%3Ddce6fb5f608e4e8ca7e1f7fd5a94aeed52e09bcf%7Ctr%3Dsls%7Csn%3D803618%7Chk%3D1e25d4272f785b2bd112b3ff0e4fc5e53aa0ebe5',
+      '_blank'
+    );
+  }
+
   return (
-    <li className="w-25 h-34 flex flex-col flex-shrink-0">
-      <img src={itemImg} alt="샘플이미지" className="rounded-[8px]" />
+    <li className="w-25 h-34 flex flex-col flex-shrink-0" onClick={handleLink}>
+      <img src={itemImage} alt="샘플이미지" className="w-[140px] h-[90px] rounded-[8px] object-cover" />
       <h3 className="mt-[6px]">{itemName}</h3>
       <p className="mt-1 text-bold text-[12px] text-[#F26E22]">{formattedPrice}원</p>
     </li>
--- React/src/components/profile/ProductList.tsx
+++ React/src/components/profile/ProductList.tsx
@@ -1,19 +1,39 @@
 import Product from './Product';
 import { useState } from 'react';
 import { productAPI } from '../../service/fetch/api';
+import { userAPI } from '../../service/fetch/api';
 
 function ProductList() {
   const [token, setToken] = useState('');
   const [accountName, setAccountName] = useState('');
-  const [products, setProducts] = useState<any>('');
+  const [products, setProducts] = useState<any>([]);
   const [loading, setLoading] = useState(false);
+
+  // 토큰 발급 함수 -> 추후 useContext로 수정 예정
+  async function getTestToken() {
+    const email = 'tt1team@example.com'; // 테스트 계정 이메일
+    const password = 'test1team_'; // 테스트 계정 비밀번호
+    try {
+      const res = await userAPI.login(email, password);
+      const token = res.token;
+      const name = res.accountname;
+      console.log(token);
+      setToken(token);
+      setAccountName(name);
+
+      // productAPI.createProduct('서귀포 감귤 1KG', 50000, 'www.naver.com', '', token);
+    } catch (e) {
+      console.error('테스트 토큰 발급 실패:');
+      return null;
+    }
+  }
 
   // 상품 목록 조회 함수
   async function getUserProducts() {
     setLoading(true);
     try {
       const productData = await productAPI.getUserProducts(accountName, token);
-      console.log('상품 데이터');
+      console.log('상품 데이터 :', productData);
       setProducts(productData.product);
     } catch (error) {
       console.log('상품 목록 조회 실패:', error);
@@ -23,22 +43,30 @@ function ProductList() {
   }
 
   return (
-    <section className="w-full flex justify-center pl-4 py-5  bg-white">
-      <div className="flex flex-col gap-4">
-        <h2 className="font-bold">판매 중인 상품</h2>
-        <ul className="flex gap-[10px] max-w-[374px] overflow-hidden">
-          {loading ? (
-            <li>로딩 중...</li>
-          ) : (
-            products.map((product: any) => {
-              <Product itemImage={product.itemImage} itemName={product.itemName} price={product.price} />;
-            })
-          )}
-
-          {/* <Product itemName="" price={50000} /> */}
-        </ul>
-      </div>
-    </section>
+    <>
+      <button onClick={getTestToken}>토큰 받기</button>
+      <button onClick={getUserProducts}>get User Products</button>
+      <section className={`w-full flex justify-center pl-4 py-5  bg-white ${products.length === 0 ? 'sr-only' : ''}`}>
+        <div className="flex flex-col gap-4">
+          <h2 className="font-bold">판매 중인 상품</h2>
+          <ul className="flex gap-[10px] max-w-[374px] overflow-hidden">
+            {loading ? (
+              <li>로딩 중...</li>
+            ) : (
+              products.map((product: any) => (
+                <Product
+                  key={product.id}
+                  itemImage={product.itemImage}
+                  itemName={product.itemName}
+                  price={product.price}
+                  productLink={product.link}
+                />
+              ))
+            )}
+          </ul>
+        </div>
+      </section>
+    </>
   );
 }
 
