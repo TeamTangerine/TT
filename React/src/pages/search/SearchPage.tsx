8fa4093af0f41e8ebbe8b6efa0dad248381fc4c3
MinKyeongHyeon <28599@naver.com>
2025-09-18 18:46:59 +0900

    feat/ search 페이지 기능 구현

--- React/src/pages/search/SearchPage.tsx
+++ React/src/pages/search/SearchPage.tsx
@@ -1,16 +1,73 @@
+import { useEffect, useState } from 'react';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/Header';
 import UserProfile from '../../components/UserProfile';
+import useDebounce from '../../Utils/debouncer';
+import { userAPI } from '../../service/fetch/api';
+import { UserAPI } from '../../types/IFetchType';
 
 function SearchPage() {
+  //todo keyword (API호출) 서치 기능 구현
+  // 1. 인풋(헤어)에서 값을 가져와서 상태 저장 (디바운싱 고려)
+  // 2. 스타일 수정
+  // 3. 서치 API를 통해 (searchUser) 키워드 값 대입
+  // 4. 결과를 렌더
+
+  //input에서 가져온 밸류
+  const [inputValue, setInputValue] = useState('');
+  //검색 결과
+  const [searchResults, setSearchResults] = useState<UserAPI.ISearchUserResponse>([]);
+  //에러 메세지
+  const [error, setError] = useState('');
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+  //디바운스
+  const keyword = useDebounce(inputValue, 500);
+
+  //서치 api호출
+  async function searchUseData(searchKeyword: string) {
+    try {
+      const res = await userAPI.searchUser(searchKeyword);
+      setSearchResults(res);
+    } catch (error: any) {
+      console.log(error);
+      error && setError(error.message);
+    }
+  }
+
+  //디바운스 값이 변경될때 api통신
+  useEffect(() => {
+    if (keyword.trim()) {
+      searchUseData(keyword);
+    }
+  }, [keyword, error]);
+
   return (
     <>
-      <Header navStyle="top-search" />
-      <ul className="flex flex-col gap-4 mt-5 mx-4">
-        <UserProfile username="애월읍 위니브 감귤농장" accountname="@ weniv_Mandarin" />
-        <UserProfile username="애월읍 한라봉 최고 맛집" accountname="@ hanlabong" />
-        <UserProfile username="감귤의 품격 - 애월읍" accountname="@ mandarin_king" />
-      </ul>
+      <Header navStyle="top-search" searchOnChange={handleOnChange} inputValue={inputValue} />
+      <main className="flex justify-center">
+        <div className="w-[390px] flex flex-col gap-4 mt-5 mx-4">
+          {searchResults.length > 0 ? (
+            searchResults.map((user) => (
+              <UserProfile
+                key={user._id}
+                username={user.username}
+                accountname={user.accountname}
+                image={user.image}
+                hover={true}
+              />
+            ))
+          ) : inputValue.trim() ? (
+            // 검색어는 있지만 결과가 없을 때
+            <p className="text-center text-sm text-gray-500">{error}</p>
+          ) : (
+            // 초기 상태(검색어 입력 전)
+            <p className="text-center text-sm text-gray-400">검색어를 입력해주세요.</p>
+          )}
+        </div>
+      </main>
       <Footer />
     </>
   );
