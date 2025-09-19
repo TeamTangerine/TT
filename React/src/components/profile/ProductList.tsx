14d8d4973e41b3e99194945208f4264ceb317e45
Liebe_Chaeso <d131215@naver.com>
2025-09-19 17:25:12 +0900

    fix/product-list 레이아웃 안 맞는 부분 수정

--- React/src/components/profile/ProductList.tsx
+++ React/src/components/profile/ProductList.tsx
@@ -30,6 +30,7 @@ function ProductList({ isOwner }: ProductListProps) {
       const productData = await productAPI.getUserProducts(accountName);
       // product에 productData.product데이터 저장
       setProducts(productData.product);
+      console.log(productData.product);
     } catch (error) {
       console.log('상품 목록 조회 실패:', error);
     } finally {
@@ -39,32 +40,32 @@ function ProductList({ isOwner }: ProductListProps) {
 
   useEffect(() => {
     getUserInfo();
+    getUserProducts();
   }, []);
 
   return (
     <>
-      <button onClick={getUserProducts}>get User Products</button>
-      <section className={`w-full flex justify-center pl-4 py-5  bg-white ${products.length === 0 ? 'hidden' : ''}`}>
-        <div className="flex flex-col gap-4">
-          <h2 className="font-bold">판매 중인 상품</h2>
-          <ul className="flex gap-[10px] w-[390px] overflow-hidden overflow-x-auto">
-            {loading ? (
-              <li>로딩 중...</li>
-            ) : (
-              products.map((product) => (
-                <Product
-                  key={product.id}
-                  itemImage={product.itemImage}
-                  itemName={product.itemName}
-                  price={product.price}
-                  productLink={product.link}
-                  isOwner={isOwner}
-                  setShowModal={setShowModal}
-                />
-              ))
-            )}
-          </ul>
-        </div>
+      <section
+        className={`flex flex-col gap-4 justify-center w-full h-[208px] pl-4 py-5 bg-white ${products.length === 0 ? 'hidden' : ''}`}
+      >
+        <h2 className="font-bold h-[20px]">판매 중인 상품</h2>
+        <ul className="flex gap-[10px] overflow-hidden overflow-x-auto">
+          {loading ? (
+            <li>로딩 중...</li>
+          ) : (
+            products.map((product) => (
+              <Product
+                key={product.id}
+                itemImage={product.itemImage}
+                itemName={product.itemName}
+                price={product.price}
+                productLink={product.link}
+                isOwner={isOwner}
+                setShowModal={setShowModal}
+              />
+            ))
+          )}
+        </ul>
       </section>
       {showModal && <Modal showModal={showModal} closeModal={() => setShowModal(false)} />}
     </>
