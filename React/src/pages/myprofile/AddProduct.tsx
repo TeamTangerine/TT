b5ec680203433642fd532e2f774b61e205feb3a9
MinKyeongHyeon <28599@naver.com>
2025-09-19 17:48:23 +0900

    delete/중복된 속성 제거

--- React/src/pages/myprofile/AddProduct.tsx
+++ React/src/pages/myprofile/AddProduct.tsx
@@ -168,7 +168,7 @@ function AddProduct() {
               alt=""
               className="w-[322px] h-[204px] rounded-[10px] bg-[#DBDBDB] object-cover "
             />
-            <div className="absolute bottom-3 right-3 right-3 w-9 h-9 rounded-2xl flex items-center justify-center bg-[#C4C4C4]">
+            <div className="absolute bottom-3 right-3 w-9 h-9 rounded-2xl flex items-center justify-center bg-[#C4C4C4]">
               <img src={uploadImg} alt="" />
             </div>
             <input onChange={handleFileChange} type="file" id="img" name="img" accept="image/*" className="hidden" />
