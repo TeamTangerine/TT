af55463de715a30efe2c6c79270e9296eddad114
gyeone <voov000@naver.com>
2025-09-20 20:30:40 +0900

    feat/페이지 이동 시 url에 파라미터 추가와 state넘겨주기

--- React/src/components/Posting.tsx
+++ React/src/components/Posting.tsx
@@ -114,7 +114,8 @@ function Posting({
                 </button>
                 <span className="text-[12px] text-[#767676]">{heartCount}</span>
               </div>
-              <div className="flex gap-[6px] items-center" onClick={() => navigate('/post')}>
+              {/* 댓글 버튼이나 댓글수 눌렀을 때 상세 페이지로 이동 */}
+              <div className="flex gap-[6px] items-center" onClick={() => navigate(`/post/${id}`, { state: { post } })}>
                 <button className="w-5 h-5">
                   <img src={iconMessage} alt="댓글" />
                 </button>
