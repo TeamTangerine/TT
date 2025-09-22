0d2dc3c24bdfdbfa6b450473098ea5a68a86ff2b
gyeone <voov000@naver.com>
2025-09-22 15:54:02 +0900

    feat/Comment 컴포넌트 에서 props설정하고 받아와서 적용하기(날짜는 아직 미완성)

--- React/src/pages/post/components/Comment.tsx
+++ React/src/pages/post/components/Comment.tsx
@@ -1,21 +1,32 @@
 import profileImg from '../../../assets/Ellipse 6.png';
 import MoreBtn from '../../../assets/icon/s-icon-more-vertical.png';
 
-function Comment() {
+type CommentProps = {
+  userProfileImage: string;
+  userName: string;
+  content: string;
+  createdAt: string;
+};
+
+function Comment({ userProfileImage, userName, content, createdAt }: CommentProps) {
   return (
     <li className="flex gap-3 items-start w-full justify-between ">
-      <img className="w-9 h-9 rounded-full" src={profileImg} alt="댓글 작성자 프로필 이미지" />
+      <img
+        className="w-9 h-9 rounded-full"
+        src={userProfileImage === '/Elipse.png' ? profileImg : userProfileImage}
+        alt="댓글 작성자 프로필 이미지"
+      />
       <div className="flex flex-col justify-between flex-1 gap-[16px]">
         <div className="flex justify-between pt-[5px]">
           <div className="flex justify-between items-center gap-[6px]">
-            <h3 className="text-[14px] font-medium">서귀포시 무슨 농장</h3>
-            <p className="text-[10px] text-[#767676] ">· 5분 전</p>
+            <h3 className="text-[14px] font-medium">{userName}</h3>
+            <p className="text-[10px] text-[#767676] ">{createdAt}</p>
           </div>
           <button type="button">
             <img className="w-5 h-5" src={MoreBtn} alt="더보기" />
           </button>
         </div>
-        <p className="text-[14px] text-[#333333]">게시글 답글 ~~ !! 최고최고</p>
+        <p className="text-[14px] text-[#333333]">{content}</p>
       </div>
     </li>
   );
