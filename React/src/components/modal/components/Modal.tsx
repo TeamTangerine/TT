cac7951f205ed8b5c156766fb8ec2cf1c0fda90f
MinKyeongHyeon <28599@naver.com>
2025-09-22 21:15:31 +0900

    feat/모달 기능 분리를 시도(설정을 분리함)

--- React/src/components/modal/components/Modal.tsx
+++ React/src/components/modal/components/Modal.tsx
@@ -1,33 +1,51 @@
 import { createPortal } from 'react-dom';
-import { useNavigate } from 'react-router-dom';
 
 interface ModalProps {
   isOpen: boolean;
   isClose: () => void;
+  message: string;
+  rightLabel: string;
+  action: () => void;
 }
-
-function Modal({ isOpen, isClose }: ModalProps) {
-  const navigate = useNavigate();
-  const logout = () => {
-    localStorage.removeItem('TOKEN_KEY');
-    alert('로그아웃 완료');
-    navigate('/login');
-  };
+/**
+ *
+ * @param isOpen -boolean 열렸는지 확인만 관여
+ * @param
+ * @returns
+ */
+function Modal({ isOpen, isClose, message, rightLabel, action }: ModalProps) {
   if (!isOpen) {
     return null;
   }
+
+  const handleConfirm = () => {
+    action();
+    isClose();
+  };
+
   return createPortal(
-    <dialog className="fixed bottom-1/2 flex flex-col justify-end w-[252px] h-[110px] rounded-[10px] bg-white border border-[#DBDBDB] z-50">
-      <p className="mb-[22px] text-[16px] text-center">로그아웃 하시겠어요?</p>
-      <div className="flex">
-        <button type="button" onClick={isClose} className="w-[125px] h-[46px] border-t border-r border-[#DBDBDB]">
-          취소
-        </button>
-        <button type="button" onClick={logout} className="w-[126px] h-[46px] border-t border-[#DBDBDB] text-[#F26E22]">
-          로그아웃
-        </button>
+    <>
+      {/* 모달 전용 백드롭 (배경) */}
+      <div className="fixed inset-0 z-40 bg-black bg-opacity-50" onClick={isClose} />
+
+      <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[252px] h-[110px] rounded-[10px] bg-white border border-[#DBDBDB] z-50 flex flex-col justify-center">
+        <div className="h-[64px] flex flex-col justify-center">
+          <p className=" text-[16px] text-center">{message}</p>
+        </div>
+        <div className="flex">
+          <button type="button" onClick={isClose} className="w-[125px] h-[46px] border-t border-r border-[#DBDBDB]">
+            취소
+          </button>
+          <button
+            type="button"
+            onClick={handleConfirm}
+            className="w-[126px] h-[46px] border-t border-[#DBDBDB] text-[#F26E22]"
+          >
+            {rightLabel}
+          </button>
+        </div>
       </div>
-    </dialog>,
+    </>,
     document.body
   );
 }
