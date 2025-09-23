d464edb6e5f4f52882c490740be27e0a6d9a8cad
gyeone <voov000@naver.com>
2025-09-23 10:33:05 +0900

    feat/댓글 작성 시간을 현재 시간과 비교하여 적용

--- React/src/pages/post/components/Comment.tsx
+++ React/src/pages/post/components/Comment.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import profileImg from '../../../assets/Ellipse 6.png';
 import MoreBtn from '../../../assets/icon/s-icon-more-vertical.png';
 import Modal from '../../../components/modal/Modal';
+import { elapsedTime } from '../../../Utils/convertTime';
 
 type CommentProps = {
   userProfileImage: string;
@@ -15,7 +16,7 @@ function Comment({ userProfileImage, userName, content, createdAt }: CommentProp
 
   return (
     <>
-      <li className="flex gap-3 items-start w-[390px] justify-between ">
+      <li className="flex gap-3 items-start w-[358px] justify-between ">
         <img
           className="w-9 h-9 rounded-full"
           src={userProfileImage === '/Elipse.png' ? profileImg : userProfileImage}
@@ -25,7 +26,7 @@ function Comment({ userProfileImage, userName, content, createdAt }: CommentProp
           <div className="flex justify-between pt-[5px]">
             <div className="flex justify-between items-center gap-[6px]">
               <h3 className="text-[14px] font-medium">{userName}</h3>
-              <p className="text-[10px] text-[#767676] ">{createdAt}</p>
+              <p className="text-[10px] text-[#767676] ">{elapsedTime(createdAt)}</p>
             </div>
             <button type="button">
               <img className="w-5 h-5" onClick={() => setShowModal(true)} src={MoreBtn} alt="더보기" />
@@ -34,7 +35,7 @@ function Comment({ userProfileImage, userName, content, createdAt }: CommentProp
           <p className="text-[14px] text-[#333333]">{content}</p>
         </div>
       </li>
-      {showModal && <Modal showModal={showModal} closeModal={() => setShowModal(false)} />}
+      {/* showModal && <Toast toastStyle='commentId' showModal={showModal} closeModal={() => setShowModal(false)} /> */}
     </>
   );
 }
