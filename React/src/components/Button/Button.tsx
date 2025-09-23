97a98807138e91af6ae6da14f081f33d3c0b2729
MinKyeongHyeon <28599@naver.com>
2025-09-23 15:35:53 +0900

    delete/의미없는 빈태그(Fragments)삭제

--- React/src/components/Button/Button.tsx
+++ React/src/components/Button/Button.tsx
@@ -32,21 +32,16 @@ export default function Button({
   onClick,
 
   btnForm,
-
 }: IBtnType) {
   return (
-    <>
-      <button
-        type={btnType}
-        className={`flex justify-center items-center py-[14px] px-[11px] font-medium ${getButtonSize(btnSize)} ${getButtonColor(btnColor)} ${btnFlexBasis}`}
-        onClick={onClick}
-
-        disabled={btnColor === 'disable' ? true : false}
-        form={btnForm}
-
-      >
-        {btnTextContent}
-      </button>
-    </>
+    <button
+      type={btnType}
+      className={`flex justify-center items-center py-[14px] px-[11px] font-medium ${getButtonSize(btnSize)} ${getButtonColor(btnColor)} ${btnFlexBasis}`}
+      onClick={onClick}
+      disabled={btnColor === 'disable' ? true : false}
+      form={btnForm}
+    >
+      {btnTextContent}
+    </button>
   );
 }
