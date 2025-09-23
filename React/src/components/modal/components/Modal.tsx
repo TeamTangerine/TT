1edd4c630af6ce07d27704cab73dd16eb61aaa44
MinKyeongHyeon <28599@naver.com>
2025-09-23 13:45:27 +0900

    style/ z-index 순서 수정, 모달 폰트 굵기 조정

--- React/src/components/modal/components/Modal.tsx
+++ React/src/components/modal/components/Modal.tsx
@@ -26,11 +26,11 @@ function Modal({ isOpen, isClose, message, rightLabel, action }: ModalProps) {
   return createPortal(
     <>
       {/* 모달 전용 백드롭 (배경) */}
-      <div className="fixed inset-0 z-40 bg-black bg-opacity-50" onClick={isClose} />
+      <div className="fixed inset-0 z-30 bg-black bg-opacity-50" onClick={isClose} />
 
-      <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[252px] h-[110px] rounded-[10px] bg-white border border-[#DBDBDB] z-50 flex flex-col justify-center">
+      <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[252px] h-[110px] rounded-[10px] bg-white border border-[#DBDBDB] z-40 flex flex-col justify-center">
         <div className="h-[64px] flex flex-col justify-center">
-          <p className=" text-[16px] text-center">{message}</p>
+          <p className=" text-[16px] text-center font-medium">{message}</p>
         </div>
         <div className="flex">
           <button type="button" onClick={isClose} className="w-[125px] h-[46px] border-t border-r border-[#DBDBDB]">
