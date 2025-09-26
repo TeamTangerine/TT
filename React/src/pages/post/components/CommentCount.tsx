edfc72f69acbc5e925ad56e0f723653450799956
gyeone <voov000@naver.com>
2025-09-26 19:33:22 +0900

    feat/댓글 갯수가 늘어나도록 해주는 함수 구현

---
+++ React/src/pages/post/components/CommentCount.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+
+/**
+ * 해당 게시물에 댓글을 달면 자동으로 댓글 수만 렌더링 되어 댓글 갯수가 늘어나도록 해주는 함수입니다.
+ * @param commentCount - 해당 게시물의 댓글 수
+ */
+
+function CommentCount(commentCount: number) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setCount(commentCount);
+  }, [commentCount]);
+
+  return <span className="text-[12px] text-[#767676]">{count}</span>;
+}
+
+export default CommentCount;
