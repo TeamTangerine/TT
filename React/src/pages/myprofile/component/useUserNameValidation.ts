f864c3b1425d5787f85aa952516261c2f0cad887
MinKyeongHyeon <28599@naver.com>
2025-09-30 23:55:13 +0900

    refactor/커스텀 훅 생성(유저이름)

---
+++ React/src/pages/myprofile/component/useUserNameValidation.ts
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { validateUserName } from '../../../utils/validation';
+
+export function useUserNameValidation(initialValue = '') {
+  const [userName, setUserName] = useState(initialValue);
+  const [isNameValid, setIsNameValid] = useState(true);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setUserName(value);
+    setIsNameValid(validateUserName(value));
+  };
+
+  return { userName, setUserName, isNameValid, handleChange };
+}
