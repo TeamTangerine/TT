1e2286af7316d652b90de2e133bb43eb100f4732
MinKyeongHyeon <28599@naver.com>
2025-10-01 21:56:35 +0900

    refactor/사용안하는 프레그먼트 제거

--- React/src/pages/Auth/components/SNSLogin.tsx
+++ React/src/pages/Auth/components/SNSLogin.tsx
@@ -9,17 +9,15 @@ type EmailLoginProps = {
 
 function SNSLogin({ name, logoImg, borderColor, altText }: EmailLoginProps) {
   return (
-    <>
-      <button
-        className={`relative w-[322px] h-[44px] rounded-[44px] border-[1px] ${borderColor} text-[14px] text-[#767676] text-center flex items-center justify-center cursor-pointer`}
-        onClick={() => {
-          alert('해당 서비스는 개발 예정 중에 있습니다.');
-        }}
-      >
-        <img src={logoImg} alt={altText} className="absolute left-[14px] w-[24px] h-[24px]" />
-        <span>{name} 계정으로 로그인</span>
-      </button>
-    </>
+    <button
+      className={`relative w-[322px] h-[44px] rounded-[44px] border-[1px] ${borderColor} text-[14px] text-[#767676] text-center flex items-center justify-center cursor-pointer`}
+      onClick={() => {
+        alert('해당 서비스는 개발 예정 중에 있습니다.');
+      }}
+    >
+      <img src={logoImg} alt={altText} className="absolute left-[14px] w-[24px] h-[24px]" />
+      <span>{name} 계정으로 로그인</span>
+    </button>
   );
 }
 
